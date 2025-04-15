61e0f16f22668358b4e928b0d7bccb76f5dfc30a
Weeraya Watchara-amphaiwan <weeraya.watchara@gmail.com>
2025-04-16 00:26:09 +0700

    edit reservation model

--- models/Reservation.js
+++ models/Reservation.js
@@ -1,18 +1,35 @@
 const mongoose = require('mongoose');
 
 const ReservationSchema = new mongoose.Schema({
-    reservationDate: {
-        type: Date,
+    massageShop: {
+        type: String,
+        ref: 'MassageShop',
         required: true
     },
-    user: {
+    userID: {
         type: mongoose.Schema.ObjectId,
         ref: 'User',
         required: true
     },
-    massageShop: {
+    user_name: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    time: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    therapistID: {
         type: mongoose.Schema.ObjectId,
-        ref: 'MassageShop',
+        ref: 'Therapist',
         required: true
     },
     createdAt: {
@@ -22,3 +39,29 @@ const ReservationSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Reservation', ReservationSchema);
+
+
+// const mongoose = require('mongoose');
+
+// const ReservationSchema = new mongoose.Schema({
+//     reservationDate: {
+//         type: Date,
+//         required: true
+//     },
+//     user: {
+//         type: mongoose.Schema.ObjectId,
+//         ref: 'User',
+//         required: true
+//     },
+//     massageShop: {
+//         type: mongoose.Schema.ObjectId,
+//         ref: 'MassageShop',
+//         required: true
+//     },
+//     createdAt: {
+//         type: Date,
+//         default: Date.now
+//     }
+// });
+
+// module.exports = mongoose.model('Reservation', ReservationSchema);
