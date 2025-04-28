9671d55e82856e087924230c90e02d20680f29e5
Weeraya Watchara-amphaiwan <weeraya.watchara@gmail.com>
2025-04-28 23:18:06 +0700

    add JEST test

---
+++ test/US2-3.test.js
@@ -0,0 +1,37 @@
+const app = require('../server'); // <<== แก้ import ไม่มี {} แล้ว
+const request = require('supertest');
+const mongoose = require('mongoose');
+
+describe('Therapist Reservation', () => {
+  let token;
+
+  it('should log in and get token', async () => {
+    const res = await request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'therapistverified99@gmail.com',
+        password: '123456'
+      });
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.success).toBe(true); 
+    expect(res.body.token).toBeDefined();
+
+    token = res.body.token;
+  });
+
+  it('should fetch 1 reservation', async () => {
+    const res = await request(app)
+      .get('/api/v1/therapists/me/reservations') 
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.success).toBe(true); 
+    expect(res.body.count).toBe(2);
+    expect(res.body.data).toHaveLength(2);
+  });
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
